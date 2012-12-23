fca24fa7f73487ed78ad99b348ad8db83f867170
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2012-12-23 22:48:53 +0000

    Enabled mobile project. Disabled Mac App bundle. New build.

---
+++ trunk/engine/mobile/gen/R.java.d
@@ -0,0 +1,4 @@
+G:\Development\GameDev\jMP\CarGame\engine\mobile\gen\com\sunshinegamestudio\car_game\R.java \
+ : G:\Development\GameDev\jMP\CarGame\engine\mobile\res\layout\main.xml \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\res\values\strings.xml \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\AndroidManifest.xml \
---
+++ trunk/engine/mobile/gen/com/sunshinegamestudio/car_game/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sunshinegamestudio.car_game;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = false;
+}
