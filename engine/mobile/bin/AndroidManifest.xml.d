a38a73189bb26244eecdf25e6bacaa6f87d61972
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2012-12-23 22:36:23 +0000

    Disabled mobile project. New build.

--- engine/mobile/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\AndroidManifest.xml : \
-G:\Development\GameDev\jMP\CarGame\engine\mobile\AndroidManifest.xml \
--- engine/mobile/bin/CarGame.ap_.d
+++
@@ -1,4 +0,0 @@
-G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\CarGame.ap_ \
- : G:\Development\GameDev\jMP\CarGame\engine\mobile\res\layout\main.xml \
-G:\Development\GameDev\jMP\CarGame\engine\mobile\res\values\strings.xml \
-G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\AndroidManifest.xml \
