d727a5b1c5c8b675207e5d984cb1a7afab38bf23
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2013-04-19 21:45:15 +0000

    Release 0.2.1.

---
+++ 0.2.1/engine/mobile/bin/This_Is_Off_Road_Racing-release-unsigned.apk.d
@@ -0,0 +1,24 @@
+G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\This_Is_Off_Road_Racing-release-unsigned.apk : \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\This_Is_Off_Road_Racing.ap_ \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\bin\classes.dex \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-jogg.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-android.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-plugins.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\This_Is_Off_Road_Racing.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\nifty-default-controls.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-niftygui.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-core.jar \
+F:\Program Files (x86)\Android\android-sdk\tools\support\annotations.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\j-ogg-vorbisd.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\nifty-style-black.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\j-ogg-oggd.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\eventbus.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\assets.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-bullet.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-terrain.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-effects.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\xmlpull-xpp3.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\jME3-networking.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\nifty.jar \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\armeabi\libbulletjme.so \
+G:\Development\GameDev\jMP\CarGame\engine\mobile\libs\armeabi-v7a\libbulletjme.so \
