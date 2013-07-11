719b7193db1bcdf3fb06a18dc8a2055a7681e424
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2013-07-11 20:56:02 +0000

    Started with implementing onClickRepeat in digital touch screen controls.

--- engine/src/cargame/gui/GameHUDScreenController_Digital.java
+++ engine/src/cargame/gui/GameHUDScreenController_Digital.java
@@ -69,6 +69,9 @@ import cargame.appstates.GameState;
 
         public void accelerate_onClickMouseMove(int x, int y)   {
         }
+        
+        public void accelerate_onClickRepeat(int x, int y)   {
+        }
 
         public void accelerate_onRelease()   {
         }
