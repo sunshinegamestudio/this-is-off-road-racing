360899abae5fe06421b27050bbcb78b209cbf70a
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2013-09-04 20:56:03 +0000

    First implementation for mobile_backup script.

---
+++ trunk/engine/mobile_backup.bat
@@ -0,0 +1,4 @@
+rem cp mobile\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java mobile_backup\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java
+rem cp mobile\AndroidManifest.xml mobile_backup\AndroidManifest.xml
+rem cp mobile\res\values\strings.xml mobile_backup\res\values\strings.xml
+rem cp mobile\ant.properties mobile_backup\ant.properties
