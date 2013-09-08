d3752c64ae6a39b70461d4f6c6e3758d73072416
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2013-09-08 20:59:12 +0000

    Enhancements for mobile_backup and mobile_restore script.

--- trunk/engine/mobile_backup.bat
+++ trunk/engine/mobile_backup.bat
@@ -1,4 +1,6 @@
-rem cp mobile\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java mobile_backup\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java
-rem cp mobile\AndroidManifest.xml mobile_backup\AndroidManifest.xml
-rem cp mobile\res\values\strings.xml mobile_backup\res\values\strings.xml
-rem cp mobile\ant.properties mobile_backup\ant.properties
+@echo off
+echo Backup custom files in mobile directory.
+copy mobile\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java mobile_backup\src\com\sunshinegamestudio\this_is_off_road_racing\MainActivity.java
+copy mobile\AndroidManifest.xml mobile_backup\AndroidManifest.xml
+copy mobile\res\values\strings.xml mobile_backup\res\values\strings.xml
+copy mobile\ant.properties mobile_backup\ant.properties
