80f2f3caa281abcb33052e72f9fd2690b05dec3f
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2014-02-12 21:05:57 +0000

    Disabled code in simpleUpdate and LicenseAcceptanceState::update() (update scene graph).

--- engine/src/cargame/appstates/LicenseAcceptanceState.java
+++ engine/src/cargame/appstates/LicenseAcceptanceState.java
@@ -129,10 +129,12 @@ public class LicenseAcceptanceState extends AbstractAppState implements ActionLi
         menuText.setText("Press [Escape] to go to the Game-State");
 
         // simple update and root node
+        /*
         rootNode.updateLogicalState(tpf);
         guiNode.updateLogicalState(tpf);
         rootNode.updateGeometricState();
         guiNode.updateGeometricState();
+        */
     }
 
     @Override
