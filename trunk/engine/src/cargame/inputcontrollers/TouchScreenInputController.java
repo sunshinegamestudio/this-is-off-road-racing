8e0a07689da8050570e8bd31c341c85fb4325b3b
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2014-07-10 21:50:53 +0000

    First implementation for InputControllers.

--- trunk/engine/src/cargame/inputcontrollers/TouchScreenInputController.java
+++ trunk/engine/src/cargame/inputcontrollers/TouchScreenInputController.java
@@ -40,6 +40,8 @@ import de.lessvoid.nifty.screen.*;
 import java.util.logging.Level;
 
 import cargame.core.CarGame;
+import cargame.gui.GameHUDScreenController_Analog;
+import cargame.gui.GameHUDScreenController_Digital;
 
 public class TouchScreenInputController extends AbstractAppState implements ActionListener{
 
@@ -89,9 +91,20 @@ public class TouchScreenInputController extends AbstractAppState implements Acti
         niftyDisplay = game.getNiftyDisplay();
         nifty = niftyDisplay.getNifty();
 
-        nifty.fromXml("General/Interface/ControllerSelectorMenu.xml", "ControllerSelectorScreen");
+        /*
+        nifty.fromXml("General/Interface/GameHUD_Analog.xml", "GameHUD");
+        GameHUDScreenController_Analog gameHUDScreenController_Analog = (GameHUDScreenController_Analog)nifty.getScreen("GameHUD").getScreenController();
+        gameHUDScreenController_Analog.setGameState(this);
+        */
 
-        // attach the nifty display to the gui view port as a processor
+        /*
+        nifty.fromXml("General/Interface/GameHUD_Digital.xml", "GameHUD");
+        GameHUDScreenController_Digital gameHUDScreenController_Digital = (GameHUDScreenController_Digital)nifty.getScreen("GameHUD").getScreenController();
+        gameHUDScreenController_Digital.setGameState(this);
+        * Use digital controls!!!
+        */
+
+            // attach the nifty display to the gui view port as a processor
         // game.getGUIViewPort().addProcessor(niftyDisplay);
     }
 
