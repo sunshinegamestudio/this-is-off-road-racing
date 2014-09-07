950866ef3eed1c69f6c7e1ebdc9ac47a50718c44
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2014-09-07 20:13:42 +0000

    Moved several things away from GameState. Added fix form removing sun/light. New build.

--- trunk/engine/src/cargame/appstates/LapTimesState.java
+++ trunk/engine/src/cargame/appstates/LapTimesState.java
@@ -42,6 +42,7 @@ import java.util.logging.Level;
 import cargame.core.CarGame;
 import cargame.entities.StartingPoint;
 import cargame.entities.SimpleCarPlayer;
+import cargame.other.Time;
 
 public class LapTimesState extends AbstractAppState    {
 
@@ -51,17 +52,38 @@ public class LapTimesState extends AbstractAppState    {
     private Node guiNode;
 
     private CarGame game = null;
-
-    // private StartingPoint startingPoint;
-    // private SimpleCarPlayer player;
     
-    // private boolean isOnStartingPoint = true;
+    private StartingPointState startingPointState;
+
+    private BitmapText fpsText;
+    private BitmapText menuText;
+    private BitmapText currentLapText;
+    private BitmapText maxLapsText;
+    private BitmapText fastestLapTimeText;
+    private BitmapText currentLapTimeText;
+    private BitmapText currentTimeText;
+    private BitmapFont guiFont;
+
+    private boolean newLap = false;
+    private int currentLap = 0;
+    private int maxLaps = 4;
+    // private int maxLaps = 1;
+    private long fastestLapTime = 0;
+    private Time fastestLapTimeTime;
+    private long currentLapTime = 0;
+    private Time currentLapTimeTime;
+    private long lastLapTime = 0;
+    private long currentTime = 0;
+    private Time currentTimeTime;
+    private long lapTimes[];
     
     public LapTimesState(CarGame game) {
     	this.game = game;
 
         rootNode = this.game.getRootNode();
 	guiNode = this.game.getGuiNode();
+        
+        startingPointState = game.getStateManager().getState(StartingPointState.class);
         
         this.game.getLogger().log(Level.SEVERE, "TrackSelectorState created.");
     }
@@ -70,21 +92,130 @@ public class LapTimesState extends AbstractAppState    {
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
 
-        // startingPoint = new StartingPoint(game.getAssetManager(), rootNode, game.getPhysicsSpace(), game.getCamera());
+        loadText();
+        
+        currentLap = 0;
+        lapTimes = new long[10];
+        for(int i=0; i<maxLaps; i++)    {
+            lapTimes[i] = 0;
+        }
+        fastestLapTimeTime = new Time();
+        currentLapTimeTime = new Time();
+        currentTimeTime = new Time();
+
+        // Load the fastest laptime (still to implement !!!)
+        /*
+        try {
+        trackStatistics = (TrackStatistics) SaveGame.loadGame("com/sunshinegamestudio/cargame", "Default_Track");
+        } catch (NullPointerException nullPointerException) {
+            trackStatistics = new TrackStatistics();
+            trackStatistics.setFastestLapTime(fastestLapTime);
+            SaveGame.saveGame("com/sunshinegamestudio/cargame", "Default_Track", trackStatistics);
+        }
+        trackStatistics = (TrackStatistics) SaveGame.loadGame("com/sunshinegamestudio/cargame", "Default_Track");
+        fastestLapTime = trackStatistics.getFastestLapTime();
+         * Temperairy disabled until fixed
+         */
+
     }
+
+    public void loadText(){
+        /* currentLapText;
+        fastestLapTimeText;
+        currentLapTimeText;
+         * Create all the BitmapText.
+         */
+
+        guiFont = game.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+        menuText = new BitmapText(guiFont, false);
+        currentLapText = new BitmapText(guiFont, false);
+        currentLapText.setSize(guiFont.getCharSet().getRenderedSize());
+        currentLapText.setLocalTranslation(0, (game.getContext().getSettings().getHeight()/2f)-(menuText.getLineHeight()/2f)-100, 0);
+        currentLapText.setText("Current Lap: ");
+        guiNode.attachChild(currentLapText);
+
+        maxLapsText = new BitmapText(guiFont, false);
+        maxLapsText.setSize(guiFont.getCharSet().getRenderedSize());
+        maxLapsText.setLocalTranslation(0, (game.getContext().getSettings().getHeight()/2f)-(menuText.getLineHeight()/2f)-80, 0);
+        maxLapsText.setText("Max Laps: ");
+        guiNode.attachChild(maxLapsText);
+
+        fastestLapTimeText = new BitmapText(guiFont, false);
+        fastestLapTimeText.setSize(guiFont.getCharSet().getRenderedSize());
+        fastestLapTimeText.setLocalTranslation(0, (game.getContext().getSettings().getHeight()/2f)-(menuText.getLineHeight()/2f)-60, 0);
+        fastestLapTimeText.setText("Fastest Lap: ");
+        guiNode.attachChild(fastestLapTimeText);
+        
+        currentLapTimeText = new BitmapText(guiFont, false);
+        currentLapTimeText.setSize(guiFont.getCharSet().getRenderedSize());
+        currentLapTimeText.setLocalTranslation(0, (game.getContext().getSettings().getHeight()/2f)-(menuText.getLineHeight()/2f)-40, 0);
+        currentLapTimeText.setText("Current Lap: ");
+        guiNode.attachChild(currentLapTimeText);
+
+        currentTimeText = new BitmapText(guiFont, false);
+        currentTimeText.setSize(guiFont.getCharSet().getRenderedSize());
+        currentTimeText.setLocalTranslation(0, (game.getContext().getSettings().getHeight()/2f)-(menuText.getLineHeight()/2f)-20, 0);
+        currentTimeText.setText("Current Time: ");
+        guiNode.attachChild(currentTimeText);
+    }
+
+    /*
+    private void loadMenu() {
+    }
+        
+    }
+    */
 
     @Override
     public void update(float tpf) {
         super.update(tpf);
 
-        // Set isOnStartingPoint to false, only if the player was already on startingPoint
-        /*
-        if(isOnStartingPoint == true)   {
-            if(startingPoint.isOnStartinPoint() == false)  {
-                isOnStartingPoint = false;
+        // Check for new lap
+        newLap = startingPointState.checkForNewLap();
+        
+        if (newLap == true)
+        {
+            // Calculate the current laptime
+            currentLapTime = game.getTimer().getTime() - lastLapTime;
+            
+            // Set the current currentLapTime in the lapTimes array
+            lapTimes[currentLap]=currentLapTime;
+            
+            // Check if there is a new fastest laptime
+            if ((fastestLapTime > currentLapTime) || (currentLap == 0))
+            {
+                // Make the current laptime the fastest laptime
+                fastestLapTime = currentLapTime;
+                // Save the fastest laptime (still to implement !!!)
+                /*
+                trackStatistics.setFastestLapTime(fastestLapTime);
+                SaveGame.saveGame("com/sunshinegamestudio/cargame", "Default_Track", trackStatistics);
+                 * Temperairy disabled until fixed
+                 */
             }
+
+            // Set the last laptime and increment the current lap
+            lastLapTime = currentLapTime;
+            currentLap = currentLap + 1;
         }
-        */
+        
+        // Update current time
+        currentTime = game.getTimer().getTime() - currentLapTime;
+        
+        // Update HUD
+        currentLapText.setText("Current Lap: " + currentLap);
+        maxLapsText.setText("Max Laps: " + maxLaps);
+        fastestLapTimeTime.setTime(fastestLapTime / game.getTimer().getResolution());
+        // fastestLapTimeText.setText("Fastest LapTime: " + fastestLapTime);
+        fastestLapTimeText.setText("Fastest LapTime: " + fastestLapTimeTime.getHour() + ":" + fastestLapTimeTime.getMinute() + ":" + fastestLapTimeTime.getSecond() + ":" + fastestLapTimeTime.getMilisecond());
+        currentLapTimeTime.setTime(currentLapTime / game.getTimer().getResolution());
+        // currentLapTimeText.setText("Current LapTime: " + currentLapTime);
+        currentLapTimeText.setText("Current LapTime: " + currentLapTimeTime.getHour() + ":" + currentLapTimeTime.getMinute() + ":" + currentLapTimeTime.getSecond() + ":" + currentLapTimeTime.getMilisecond());
+        currentTimeTime.setTime(currentTime / game.getTimer().getResolution());
+        // currentTimeText.setText("Current Time: " + currentTime);
+        currentTimeText.setText("Current Time: " + currentTimeTime.getHour() + ":" + currentTimeTime.getMinute() + ":" + currentTimeTime.getSecond() + ":" + currentTimeTime.getMilisecond());
+
+
     }
     
     @Override
@@ -95,15 +226,13 @@ public class LapTimesState extends AbstractAppState    {
     public void cleanup() {
         super.cleanup();
     }
-
-    /*
-    public boolean checkForNewLap()   {
-        if((isOnStartingPoint == false) && (startingPoint.isOnStartinPoint() == true))    {
-            isOnStartingPoint = true;
-            return true;
-        }   else    {
-            return false;
-        }
+    
+    public int getCurrentLap()  {
+        return currentLap;
     }
-    */
+    
+    public int getMaxLaps() {
+        return maxLaps;
+    }
+
 }
