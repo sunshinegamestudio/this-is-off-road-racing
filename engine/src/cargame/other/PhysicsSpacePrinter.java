d7b7617ae4baf7600b6f4fd66d324fc3ae86674d
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2014-09-20 20:09:44 +0000

    First implementation of PhysicsSpacePrinter and SceneGraphPrinter.

---
+++ engine/src/cargame/other/PhysicsSpacePrinter.java
@@ -0,0 +1,46 @@
+/*
+This Is Off Road Racing, a game where you can drive off road.
+Copyright (C) 2010 - 2013  Sunshine GameStudio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cargame.other;
+
+import cargame.core.CarGame;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+
+public class PhysicsSpacePrinter {
+    CarGame game;
+    PhysicsSpace physicsSpace;
+    
+    public PhysicsSpacePrinter(CarGame game)  {
+    	this.game = game;
+        this.physicsSpace = game.getPhysicsSpace();
+    }
+    
+    public void print()    {
+        List<PhysicsRigidBody> rigidBodyControls = (List<PhysicsRigidBody>) physicsSpace.getRigidBodyList();
+        Iterator it = rigidBodyControls.listIterator();
+        while(it.hasNext()) {
+            PhysicsRigidBody physicsRigidBody = (PhysicsRigidBody) it.next();
+            game.getLogger().log(Level.SEVERE, physicsRigidBody.toString());
+        }
+        
+    }
+}
