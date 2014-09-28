1a1645985a5c6b23d4b074f5c97da4f38276b729
sunshinegamestudio <sunshinegamestudio@dfbc0a8f-5aca-6c67-0c2f-5d0e568a03f4>
2014-09-28 20:58:11 +0000

    Improvements for track "Beach Resort". New build.

--- engine/src/cargame/controls/BoxCollisionShapeControl.java
+++ engine/src/cargame/controls/BoxCollisionShapeControl.java
@@ -26,12 +26,16 @@ import com.jme3.bullet.collision.PhysicsCollisionListener;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.control.GhostControl;
 import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
 import com.jme3.export.Savable;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.control.Control;
+import java.io.IOException;
 
 /**
  *
@@ -81,4 +85,25 @@ public class BoxCollisionShapeControl extends AbstractControl implements Savable
     protected void controlRender(RenderManager rm, ViewPort vp) {
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public Control cloneForSpatial(Spatial spatial){
+        final BoxCollisionShapeControl control = new BoxCollisionShapeControl();
+        /* Optional: use setters to copy userdata into the cloned control */
+        // control.setIndex(i); // example
+        control.setSpatial(spatial);
+        return control;
+    }
+
+    @Override
+      public void read(JmeImporter im) throws IOException {
+          super.read(im);
+          // im.getCapsule(this).read(...);
+      }
+
+      @Override
+      public void write(JmeExporter ex) throws IOException {
+          super.write(ex);
+          // ex.getCapsule(this).write(...);
+      }
 }
