4340a023db9f41736aec1ed213cd905581f6b957
Sunshine GameStudio <sunshinegamestudio@users.noreply.github.com>
2016-01-24 22:00:46 +0100

    New build.

--- engine/mobile/gen/R.java.d
+++ engine/mobile/gen/R.java.d
@@ -5,4 +5,8 @@ H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\
 H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\res\drawable-xhdpi\ic_launcher.png \
 H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\res\layout\main.xml \
 H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\res\values\strings.xml \
+H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\bin\res\drawable-hdpi\ic_launcher.png \
+H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\bin\res\drawable-ldpi\ic_launcher.png \
+H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\bin\res\drawable-mdpi\ic_launcher.png \
+H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\bin\res\drawable-xhdpi\ic_launcher.png \
 H:\Development\GameDev\jMP\CarGame\master\this-is-off-road-racing\engine\mobile\bin\AndroidManifest.xml \
