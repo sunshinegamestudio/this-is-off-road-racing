f8e903e39ff632fb5f80510c6b60162305dabd2d
Sunshine GameStudio <sunshinegamestudio@users.noreply.github.com>
2017-01-15 20:13:49 +0100

    Initial implementation for shadows (partially).

---
+++ engine/src/cargame/appstates/ShadowState.java
@@ -0,0 +1,115 @@
+/*
+This Is Off Road Racing, a game where you can drive off road.
+Copyright (C) 2010 - 2013  Sunshine GameStudio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cargame.appstates;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.RenderState;
+import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial.CullHint;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.*;
+
+import java.util.logging.Level;
+
+import cargame.core.CarGame;
+import cargame.entities.SimpleCarPlayer;
+import com.jme3.light.DirectionalLight;
+import com.jme3.light.Light;
+import com.jme3.renderer.ViewPort;
+
+public class ShadowState extends AbstractAppState implements CleanupManualInterface    {
+
+    // protected Node rootNode = new Node("Root Node");
+    private Node rootNode;
+    // protected Node guiNode = new Node("Gui Node");
+    private Node guiNode;
+
+    private SimpleCarPlayer player;
+    private Node player_node;
+    private CarGame game = null;
+
+    private DirectionalLightShadowRenderer dlsr;
+    private ViewPort viewPort;
+    private DirectionalLight directionalLight;
+
+    private boolean cleanedupManual = false;
+    
+    public ShadowState(CarGame game, ViewPort viewPort, Light light, DirectionalLight directionalLight) {
+    	this.game = game;
+        this.viewPort = viewPort;
+        this.directionalLight = directionalLight;
+
+        rootNode = this.game.getRootNode();
+	guiNode = this.game.getGuiNode();
+        
+        this.game.getLogger().log(Level.SEVERE, "TrackSelectorState created.");
+    }
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+
+        final int SHADOWMAP_SIZE=1024;
+        dlsr = new DirectionalLightShadowRenderer(game.getAssetManager(), SHADOWMAP_SIZE, 3);
+        dlsr.setLight(directionalLight);
+        viewPort.addProcessor(dlsr);
+
+        rootNode.setShadowMode(ShadowMode.Off);
+
+        cleanedupManual = false;
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+    }
+    
+    @Override
+    public void render(RenderManager rm) {
+    }
+
+    @Override
+    public void cleanupManual() {
+        // cleanup
+        cleanedupManual=true;
+    }
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+
+        if(cleanedupManual == false) {
+            cleanupManual();
+        }
+    }
+}
